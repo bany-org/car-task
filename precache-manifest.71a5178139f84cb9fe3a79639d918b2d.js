47ebe66a03a98d09d12403e9495dd562bee3869c
jakub_banasiak <soccerkid.bany@gmail.com>
2020-05-18 12:20:13 +0200

    Updates

---
+++ precache-manifest.71a5178139f84cb9fe3a79639d918b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a4e10b383f3dc45c56828fd6403567",
+    "url": "/car-task/index.html"
+  },
+  {
+    "revision": "200d217d0c63a8b0e7e1",
+    "url": "/car-task/static/js/2.76acfe9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/car-task/static/js/2.76acfe9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86d511f2557b96987e8",
+    "url": "/car-task/static/js/main.d48b936d.chunk.js"
+  },
+  {
+    "revision": "25caaac0c4a7ff979870",
+    "url": "/car-task/static/js/runtime-main.b0fc19a5.js"
+  },
+  {
+    "revision": "3cd3f3cc3a5c4637896342d9413bba72",
+    "url": "/car-task/static/media/SpraySound.3cd3f3cc.mp3"
+  }
+]);
